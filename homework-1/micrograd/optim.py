01aef240a1a41d7b4a47e7628a0a651ab73d9be0
alexvishnevskiy <46610084+alexvishnevskiy@users.noreply.github.com>
2020-09-29 15:51:45 +0300

    Add files via upload

--- homework-1/micrograd/optim.py
+++ homework-1/micrograd/optim.py
@@ -13,3 +13,16 @@ class SGD:
         for i, parameter in enumerate(self.parameters):
             self.parameters[i].grad = 0
             
+
+class StepLR:
+    def __init__(self, optimizer, step_size, gamma):
+        self.optimizer = optimizer
+        self.step_size = step_size
+        self.gamma = gamma
+        self.count_steps = 0
+        
+    def step(self):
+        self.count_steps += 1
+        if self.count_steps == self.step_size:
+            self.optimizer.lr *= self.gamma
+            self.count_steps = 0
